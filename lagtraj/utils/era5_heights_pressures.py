4c106a3ccb09aefa346f6500d01c61fd3e5c83ec
Steven Boeing <stevenboeing@localhost.localdomain>
2020-06-12 15:35:13 +0100

    Further optimisation of gradient routines

--- lagtraj/utils/era5_heights_pressures.py
+++ lagtraj/utils/era5_heights_pressures.py
@@ -692,6 +692,37 @@ def boundary_gradients(x_array, y_array, val_array):
     return x_gradient_array, y_gradient_array
 
 
+@njit
+def regression_gradients(x_array, y_array, val_array):
+    len_temp = np.shape(val_array)[0]
+    len_levels = np.shape(val_array)[1]
+    x_gradient_array = np.empty((len_temp, len_levels))
+    y_gradient_array = np.empty((len_temp, len_levels))
+    x_flat = x_array.flatten()
+    y_flat = y_array.flatten()
+    ones_flat = np.ones(np.shape(x_flat))
+    for this_time in range(len_temp):
+        for this_level in range(len_levels):
+            data_flat = val_array[
+                this_time, this_level, :, :
+            ].flatten()
+            data_flat_filter = np.expand_dims(data_flat[~np.isnan(data_flat)],axis=1)
+            x_flat_filter = np.expand_dims(x_flat[~np.isnan(data_flat)],axis=1)
+            y_flat_filter =np.expand_dims( y_flat[~np.isnan(data_flat)],axis=1)
+            ones_flat_filter = np.expand_dims(ones_flat[~np.isnan(data_flat)],axis=1)
+            oxy_mat = np.hstack((ones_flat_filter, x_flat_filter, y_flat_filter))
+            theta = np.dot(
+                np.dot(
+                    np.linalg.pinv(np.dot(oxy_mat.transpose(), oxy_mat)),
+                    oxy_mat.transpose(),
+                ),
+                data_flat_filter,
+            )
+            x_gradient_array[this_time, this_level] = theta[0][1]
+            y_gradient_array[this_time, this_level] = theta[0][2]
+    return x_gradient_array, y_gradient_array
+    
+    
 def era5_boundary_gradients(ds_box, variable, dictionary):
     """ Calculate gradients from boundary values
     Using distances along latitude and longitude axes
@@ -699,7 +730,7 @@ def era5_boundary_gradients(ds_box, variable, dictionary):
     ds_filtered = ds_box
     if "mask" in dictionary:
         mask = era5_mask(ds_box, dictionary)
-        ds_filtered = ds_filtered.where(mask)
+        ds_filtered[variable] = ds_filtered[variable].where(mask)
     lat1_point = dictionary["lat"]
     lon1_point = longitude_set_meridian(dictionary["lon"])
     lat2_mg = ds_filtered["lat_meshgrid"].values
@@ -717,7 +748,7 @@ def era5_regression_gradients(ds_box, variable, dictionary):
     ds_filtered = ds_box
     if "mask" in dictionary:
         mask = era5_mask(ds_box, dictionary)
-        ds_filtered = ds_filtered.where(mask)
+        ds_filtered[variable] = ds_filtered[variable].where(mask)
     lat1_point = dictionary["lat"]
     lon1_point = longitude_set_meridian(dictionary["lon"])
     lat2_mg = ds_filtered["lat_meshgrid"].values
@@ -725,35 +756,10 @@ def era5_regression_gradients(ds_box, variable, dictionary):
     # Use the center latitude for projection onto plane
     x_array = lon_dist(lon1_point, lon2_mg, lat1_point)
     y_array = lat_dist(lat1_point, lat2_mg)
-    x_flat = x_array.flatten()
-    y_flat = y_array.flatten()
-    ones_flat = np.ones(np.shape(x_flat))
-    len_temp = np.shape(ds_box[variable])[0]
-    len_levels = np.shape(ds_box[variable])[1]
-    x_gradient_array = np.empty((len_temp, len_levels))
-    y_gradient_array = np.empty((len_temp, len_levels))
-    for this_time in range(len_temp):
-        for this_level in range(len_levels):
-            data_flat = ds_filtered[variable][
-                this_time, this_level, :, :
-            ].values.flatten()
-            data_flat_filter = data_flat[~np.isnan(data_flat)][:, None]
-            x_flat_filter = x_flat[~np.isnan(data_flat)][:, None]
-            y_flat_filter = y_flat[~np.isnan(data_flat)][:, None]
-            ones_flat_filter = ones_flat[~np.isnan(data_flat)][:, None]
-            oxy_mat = np.hstack((ones_flat_filter, x_flat_filter, y_flat_filter))
-            theta = np.dot(
-                np.dot(
-                    np.linalg.pinv(np.dot(oxy_mat.transpose(), oxy_mat)),
-                    oxy_mat.transpose(),
-                ),
-                data_flat_filter,
-            )
-            x_gradient_array[this_time, this_level] = theta[1]
-            y_gradient_array[this_time, this_level] = theta[2]
-    return x_gradient_array, y_gradient_array
-
-
+    val_array = ds_filtered[variable].values
+    return regression_gradients(x_array, y_array, val_array)
+    
+    
 def era5_gradients(ds_field, list_of_vars, dictionary):
     """Add variables defined in list to dictionary"""
     ds_out = xr.Dataset(coords={"time": ds_field.time, "lev": ds_field.lev})
